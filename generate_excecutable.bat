18909522624e706db78e93d5b8617dc134798559
ameliasutton <ameliasutton@umass.edu>
2022-10-14 12:25:13 -0400

    Removed redundant installation commands

--- generate_excecutable.bat
+++ generate_excecutable.bat
@@ -2,8 +2,6 @@ pip install tk
 pip install psycopg2
 pip install datetime
 pip install pyinstaller
-pip install sys
-pip install os
 pyinstaller LDliteSingleSelect.py -F -n LDliteSingleSelect --distpath %cd% --workpath exe_build --noconfirm
 del LDliteSingleSelect.spec
 rmdir /Q /S exe_build
